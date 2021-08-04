3bfc210f3d37a2496037af0c351f78a04be89ae1
Vincent <vincent.philippe18@gmail.com>
2021-08-04 11:40:07 +0200

    Adding base application from dyma > Angular > Todo list

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,28 +1,28 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from './todo.model';
+import { TodoService } from './todo.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  public subscription: Subscription = new Subscription();
-  public foo: string;
-  constructor(private http: HttpClient) {}
+export class AppComponent {
+  public todos$: Observable<Todo[]> = this.todoService.todos$;
+  public message: string;
 
-  ngOnInit(): void {
-    this.subscription = this.http.get<string>('/api/sample/foo').subscribe(
-      (response: string) => {
-        this.foo = response;
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
+  constructor(private todoService: TodoService) {}
+
+  public addTodo() {
+    this.todoService.addTodo({ message: this.message, done: false });
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+  public toggleTodo(index: number) {
+    this.todoService.toggleTodo(index);
+  }
+
+  public deleteTodo(index: number) {
+    this.todoService.deleteTodo(index);
   }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
